2c2fa0f08c31c1370a3d76e0d7fe39c82ee5eab3
Benjamin DalFavero <bdalfavero@gmail.com>
2024-04-08 10:13:01 -0400

    Improved CLI.

--- scripts/plot_grads.py
+++ scripts/plot_grads.py
@@ -7,7 +7,9 @@ import pandas as pd
 import seaborn as sns
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename")
+parser.add_argument("filename", help="Filename for data input.")
+parser.add_argument("--output", help="Filename for image output.")
+parser.add_argument("--show", action="store_true", help="Shows the plot in a window.")
 args = parser.parse_args()
 
 df = pd.read_csv(args.filename, index_col="i")
@@ -16,4 +18,7 @@ fig, ax = plt.subplots()
 sns.lineplot(data=df, x="i", y="weight_grad", ax=ax)
 sns.lineplot(data=df, x="i", y="visible_grad", ax=ax)
 sns.lineplot(data=df, x="i", y="hidden_grad", ax=ax)
-plt.show()
+if args.output:
+    plt.savefig(args.output)
+if args.show:
+    plt.show()
